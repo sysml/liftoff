0d6f20d5bd507dab1ef9edd2679aec52bf0926dc
Simon Kuenzer <simon.kuenzer@neclab.eu>
2016-02-18 11:19:57 +0100

    Print child process statistics

--- liftoff.c
+++ liftoff.c
@@ -8,63 +8,91 @@
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
+#include <inttypes.h>
+#include <sys/types.h>
 #include <sys/time.h>
+#include <sys/param.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/resource.h>
 
 #define likely(x)   __builtin_expect(!(!(x)),1)
 #define unlikely(x) __builtin_expect(!(!(x)),0)
 
 int main(int argc, char *argv[])
 {
-  pid_t pid;
+  pid_t pid, caught;
   sighandler_t irqsig, quitsig;
-  struct timeval start, stop;
+  struct timeval start, elapsed;
+  struct rusage ru;
   int status;
+  FILE *sout = stdout;
 
   if (argc < 1)
     exit(1);
   if (argc == 1) {
     fprintf(stdout, "Usage: %s [PROGRAM] [[ARGUMENTS]]..\n", argv[0]);
-    fflush(stderr);
-    exit(1);
+    goto err_exit;
   }
 
-  /* disable signal handling */
-  irqsig = signal(SIGINT, SIG_IGN);
+  /* disable signal handler */
+  irqsig  = signal(SIGINT, SIG_IGN);
   quitsig = signal(SIGQUIT, SIG_IGN);
 
-  gettimeofday(&start, (struct timezone *) 0);
+  gettimeofday(&start, NULL);
+
   pid = fork();
   if (unlikely(pid < 0)) {
     fprintf(stderr, "%s: failed to fork: %s\n",
 	    argv[0], strerror(errno));
-    fflush(stderr);
-    exit(1);
+    goto err_exit;
   }
-
   if (pid == 0) {
     /* child */
     execvp(argv[1], &argv[1]); /* does not return on success */
     fprintf(stderr, "%s: failed to execute %s: %s\n",
 	    argv[0], argv[1], strerror(errno));
-    fflush(stderr);
-    exit(errno == ENOENT ? 127 : 126);    
+    goto err_exit;
+  }
+  /* wait until child closed */
+  while ((caught = wait3(&status, 0, &ru)) != pid) {
+    if (unlikely(caught == -1)) {
+      fprintf(stderr, "%s: failed to wait for child %d: %s\n",
+	      argv[0], pid, strerror(errno));
+      goto err_exit;
+    }
   }
 
-  if (waitpid(-1, &status, 0) == -1) {
-    fprintf(stderr, "%s: failed to wait for child %d: %s\n",
-	    argv[0], pid, strerror(errno));
-    fflush(stderr);
-    exit(1);
-  }
-  gettimeofday(&stop, (struct timezone *) 0);
+  gettimeofday(&elapsed, NULL);
+  timersub(&elapsed, &start, &elapsed);
 
-  /* restore signals.  */
-  signal(SIGINT, irqsig);
+  /* restore signal handler  */
+  signal(SIGINT,  irqsig);
   signal(SIGQUIT, quitsig);
 
-  fflush(stdout);
+  /* print stats */
+  fprintf(sout, "real:                         %"PRIu64".%06"PRIu64" s\n",
+	  (uint64_t) elapsed.tv_sec,
+	  (uint64_t) elapsed.tv_usec);
+  fprintf(sout, "user:                         %"PRIu64".%06"PRIu64" s\n",
+	  (uint64_t) ru.ru_utime.tv_sec,
+	  (uint64_t) ru.ru_utime.tv_usec);
+  fprintf(sout, "sys:                          %"PRIu64".%06"PRIu64" s\n",
+	  (uint64_t) ru.ru_stime.tv_sec,
+	  (uint64_t) ru.ru_stime.tv_usec);
+  fprintf(sout, "maximum resident set size:    %ld kb\n", ru.ru_maxrss);
+  fprintf(sout, "soft page faults:             %ld\n", ru.ru_minflt);
+  fprintf(sout, "hard page faults:             %ld\n", ru.ru_majflt);
+  fprintf(sout, "fs input ops:                 %ld\n", ru.ru_inblock);
+  fprintf(sout, "fs output ops:                %ld\n", ru.ru_oublock);
+  fprintf(sout, "voluntary context switches:   %ld\n", ru.ru_nvcsw);
+  fprintf(sout, "involuntary context switches: %ld\n", ru.ru_nivcsw);
 
+  fflush(sout);
   exit(0);
+
+ err_exit:
+  fflush(stdout);
+  fflush(stderr);
+  exit(1);
 }
